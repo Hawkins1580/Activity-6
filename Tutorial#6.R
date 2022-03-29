c7efed5ddf7d4a19f84d9b6984e37c9e75a95b2c
hawkinsnick <nhawkins@hamilton.edu>
2022-03-29 14:39:30 +0000

    started tutorial #6

---
+++ Tutorial#6.R
@@ -0,0 +1,22 @@
+
+# Start of Tutorial #6
+
+# read in greenhouse gas data from reservoirs
+ETdat <- read.csv("/cloud/project/activity06/ETdata.csv")
+
+
+# Installing Packages
+install.packages(c("dplyr","ggplot2","olsrr", "PerformanceAnalytics"))
+
+library(dplyr)
+library(ggplot2)
+library(olsrr)
+library(PerformanceAnalytics)
+
+
+
+
+
+
+
+
