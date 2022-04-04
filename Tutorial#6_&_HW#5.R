2e4e46f165811486c489fc0485b8859da6fa23c8
hawkinsnick <nhawkins@hamilton.edu>
2022-04-04 12:14:30 +0000

    finished coding

--- Tutorial#6_&_HW#5.R
+++ Tutorial#6_&_HW#5.R
@@ -2,10 +2,10 @@
 
 # read in greenhouse gas data from reservoirs
 ETdat <- read.csv("/cloud/project/activity06/ETdata.csv")
-
+ghg <- read.csv("/cloud/project/activity05/Deemer_GHG_Data.csv")
 
 # Installing Packages
-install.packages(c("dplyr","ggplot2","olsrr", "PerformanceAnalytics"))
+# install.packages(c("dplyr","ggplot2","olsrr", "PerformanceAnalytics"))
 # install.packages(c("lubridate", "forecast"))
 
 library(dplyr)
@@ -17,50 +17,11 @@ library(forecast)
 
 unique(ETdat$crop)
 
-# In-class pistachios
-# average fields for each month for pistachios
-pistachios <- ETdat %>% # ET data
-  filter(crop == "Pistachios") %>% # only use pisachios fields
-  group_by(date) %>% # calculate over each date
-  summarise(ET.in = mean(Ensemble.ET, na.rm=TRUE)) # average fields
-
-# visualize the data
-ggplot(pistachios, aes(x=ymd(date),y=ET.in))+
-  geom_point()+
-  geom_line()+
-  labs(x="year", y="Monthy evapotranspiration (in)")
-
-# pistachio ET time series
-pistachios_ts <- ts(pistachios$ET.in, # data
-                start = c(2016,1), #start year 2016, month 1
-                #first number is unit of time and second is observations within a unit
-                frequency= 12) # frequency of observations in a unit
-
-# decompose pistachios ET time series
-pistachios_dec <- decompose(pistachios_ts)
-# plot decomposition
-plot(pistachios_dec)
-
-# Autocorrelation
-acf(na.omit(pistachios_ts), 
-    lag.max = 24)
-
-# Autoregressive model
-pistachios_y <- na.omit(pistachios_ts)
-model1 <- arima(pistachios_y , # data 
-                order = c(1,0,0)) # first number is AR order all other numbers get a 0 to keep AR format
-model1
-
-
-
-
 # average fields for each month for almonds
 almond <- ETdat %>% # ET data
   filter(crop == "Almonds") %>% # only use almond fields
   group_by(date) %>% # calculate over each date
   summarise(ET.in = mean(Ensemble.ET, na.rm=TRUE)) # average fields
-
-
 
 # visualize the data
 ggplot(almond, aes(x=ymd(date),y=ET.in))+
@@ -158,6 +119,28 @@ ggplot() +
 # surface area, volume, precipitation, runoff
 
 ghg$NEW.co2 <- ((1)/(ghg$co2+1000))
+
+
+# log transformations 
+ghg$log.ch4 <- log(ghg$ch4+1)
+ghg$log.age <- log(ghg$age)
+ghg$log.DIP <- log(ghg$DIP+1)
+ghg$log.precip <- log(ghg$precipitation)
+ghg$log.SA <- log(ghg$surface.area)
+
+unique(ghg$Region)
+
+# binary variable for boreal region
+ghg$BorealV <- ifelse(ghg$Region == "Boreal",1,0)
+
+# binary variable for tropical region
+ghg$TropicalV <- ifelse(ghg$Region == "Tropical",1,0)
+
+# binary variable for alpine region
+ghg$AlpineV <- ifelse(ghg$Alpine == "yes",1,0)
+
+# binary variable for known hydropower
+ghg$HydroV <- ifelse(ghg$hydropower == "yes",1,0)
 
 mod.QUESTION1 <- lm(NEW.co2 ~ mean.depth+
                  log.precip+
@@ -324,12 +307,142 @@ plot(grapes_dec)
 # Question #3
 # Design an autoregressive model for pistachios
 
+PISTACHIOS.pacf.plot <- pacf(na.omit(pistachios_ts))
+
+# Running a first order AR
+pistachios_y <- na.omit(pistachios_ts)
+pistachios.model1 <- arima(pistachios_y ,  
+                order = c(1,0,0)) 
+pistachios.model1
+
+# Running a second order AR
+pistachios.model2 <- arima(pistachios_y ,  
+                order = c(2,0,0)) 
+pistachios.model2
+
+# Running a third order AR
+pistachios.model3 <- arima(pistachios_y ,  
+                order = c(3,0,0)) 
+pistachios.model3
+
+# Running a fourth order AR
+pistachios.model4 <- arima(pistachios_y ,  
+                order = c(4,0,0)) 
+pistachios.model4
+
+# Looking at fitted values
+# calculate fit
+p.AR_fit1 <- pistachios_y - residuals(pistachios.model1) 
+p.AR_fit2 <- pistachios_y - residuals(pistachios.model2)
+p.AR_fit3 <- pistachios_y - residuals(pistachios.model3)
+p.AR_fit4 <- pistachios_y - residuals(pistachios.model4)
+#plot data
+plot(pistachios_y)
+# plot fit
+points(p.AR_fit1, type = "l", col = "tomato3", lty = 2, lwd=2)
+points(p.AR_fit2, type = "l", col = "green3", lty = 2, lwd=2)
+points(p.AR_fit3, type = "l", col = "orchid2", lty = 2, lwd=2)
+points(p.AR_fit4, type = "l", col = "darkgoldenrod4", lty = 2, lwd=2)
+legend("topleft", c("data", "AR1", "AR2", "AR3", "AR4"),
+       lty=c(1,2,2), lwd=c(1,2,2), 
+       col=c("black", "tomato3", "green3", "orchid2", "darkgoldenrod4"),
+       bty="n")
+
+# Forecast using AR3
+newPistachios <- forecast(pistachios.model3)
+newPistachios
+
+#make dataframe for plotting
+newPistachiosF <- data.frame(newPistachios)
+
+# set up dates
+years <- c(rep(2021,4),rep(2022,12), rep(2023,8))
+month <- c(seq(9,12),seq(1,12), seq(1,8))
+newPistachiosF$dateF <- ymd(paste(years,"/",month,"/",1))
+
+# making a plot with data and predictions including a prediction interval
+ggplot() +
+  geom_line(data = pistachios, aes(x = ymd(date), y = ET.in))+
+  xlim(ymd(pistachios$date[1]),newPistachiosF$dateF[24])+  # Plotting original data
+  geom_line(data = newPistachiosF, aes(x = dateF, y = Point.Forecast),
+            col="red") +  # Plotting model forecasts
+  geom_ribbon(data=newPistachiosF, 
+              aes(x=dateF,ymin=Lo.95,
+                  ymax=Hi.95), fill=rgb(0.5,0.5,0.5,0.5))+ # uncertainty interval
+  theme_classic()+
+  labs(x="Year", y="Evapotranspiration (in)")
 
 
 
 # and fallow/idle fields
 
+# Design an autoregressive model for fallow / idle
 
+FALLOW.pacf.plot <- pacf(na.omit(fallow_ts))
+
+# Running a first order AR
+fallow_y <- na.omit(fallow_ts)
+fallow.model1 <- arima(fallow_y ,  
+                           order = c(1,0,0)) 
+fallow.model1
+
+# Running a second order AR
+fallow.model2 <- arima(fallow_y ,  
+                           order = c(2,0,0)) 
+fallow.model2
+
+# Running a third order AR
+fallow.model3 <- arima(fallow_y ,  
+                           order = c(3,0,0)) 
+fallow.model3
+
+# Running a fourth order AR
+fallow.model4 <- arima(fallow_y ,  
+                           order = c(4,0,0)) 
+fallow.model4
+
+# Looking at fitted values
+# calculate fit
+f.AR_fit1 <- fallow_y - residuals(fallow.model1) 
+f.AR_fit2 <- fallow_y - residuals(fallow.model2)
+f.AR_fit3 <- fallow_y - residuals(fallow.model3)
+f.AR_fit4 <- fallow_y - residuals(fallow.model4)
+#plot data
+plot(fallow_y)
+# plot fit
+points(f.AR_fit1, type = "l", col = "tomato3", lty = 2, lwd=2)
+points(f.AR_fit2, type = "l", col = "green3", lty = 2, lwd=2)
+points(f.AR_fit3, type = "l", col = "orchid2", lty = 2, lwd=2)
+points(f.AR_fit4, type = "l", col = "darkgoldenrod4", lty = 2, lwd=2)
+legend("topleft", c("data", "AR1", "AR2", "AR3", "AR4"),
+       lty=c(1,2,2), lwd=c(1,2,2), 
+       col=c("black", "tomato3", "green3", "orchid2", "darkgoldenrod4"),
+       bty="n")
+
+# Forecast using AR2
+newFallow <- forecast(fallow.model2)
+newFallow
+
+#make dataframe for plotting
+newFallowF <- data.frame(newFallow)
+
+# set up dates
+years <- c(rep(2021,4),rep(2022,12), rep(2023,8))
+month <- c(seq(9,12),seq(1,12), seq(1,8))
+newFallowF$dateF <- ymd(paste(years,"/",month,"/",1))
+
+
+# making a plot with data and predictions including a prediction interval
+ggplot() +
+  geom_line(data = fallow, aes(x = ymd(date), y = ET.in))+
+  xlim(ymd(fallow$date[1]),newFallowF$dateF[24])+  # Plotting original data
+  geom_line(data = newFallowF, aes(x = dateF, y = Point.Forecast),
+            col="red") +  # Plotting model forecasts
+  geom_ribbon(data=newFallowF, 
+              aes(x=dateF,ymin=Lo.95,
+                  ymax=Hi.95), fill=rgb(0.5,0.5,0.5,0.5))+ # uncertainty interval
+  theme_classic()+
+  labs(x="Year", y="Evapotranspiration (in)")
 
 
 
